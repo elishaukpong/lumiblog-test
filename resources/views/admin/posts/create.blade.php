c5c67948cad76fc29432f2b1dc5d90385336c9aa
Elisha Ukpong <ishukpong418@gmail.com>
2021-09-07 19:39:21 +0100

    added tages

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -6,20 +6,41 @@
     </x-slot>
 
     <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-60">
-        <form method="POST" action="{{ route('admin.comment.update', $comment->id) }}" class="py-16">
-            @method('PUT')
-            @csrf
+        <form method="POST" action="{{ route('admin.tag.store') }}" class="py-16">
+        @csrf
 
-            <input type="hidden" name="post_id" value="{{$comment->post_id}}">
+        <!-- Email Address -->
+            <div>
+                <x-label for="name" :value="__('Name')" />
+
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+            </div>
+
+            <!-- Password -->
             <div class="mt-4">
-                <x-label for="text" :value="__('Comment')" />
+                <x-label for="text" :value="__('Text')" />
 
-                <textarea name="text" rows="10" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">{{old('text') ?? $comment->text}}</textarea>
+                <x-text-area id="text" class="block mt-1 w-full" rows="10"
+                             name="text" />
+            </div>
+
+            <div class="mt-4">
+                <x-label for="text" :value="__('Tags')" />
+                <div class="mt-2 grid grid-cols-4">
+                    @foreach($tags as $tag)
+                    <div class="col-auto">
+                        <label>
+                            <input type="checkbox" value="{{$tag->id}}" name="posttag[]" class="form-checkbox">
+                            <span class="ml-2">{{$tag->name}}</span>
+                        </label>
+                    </div>
+                    @endforeach
+                </div>
             </div>
 
             <div class="flex items-center justify-end mt-4  text-center">
                 <x-button class="w-full md:inline-block">
-                    Update Comment
+                    Create Post
                 </x-button>
             </div>
         </form>
