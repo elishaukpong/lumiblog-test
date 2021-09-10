7ea51385d544fb49fffcd0447d2089b6f8b1f1d0
Elisha Ukpong <ishukpong418@gmail.com>
2021-09-10 11:35:14 +0100

    fixed error in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-dashboard>
+<x-dashboard-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -14,4 +14,4 @@
             </div>
         </div>
     </div>
-</x-dashboard>
+</x-dashboard-layout>
